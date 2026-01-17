fb2160b2e192b39083686db61660192f8efa8522
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-17 13:26:04 -0300

    feat(service): Implementa PostService com operações CRUD
    - Adiciona métodos create, read, update e delete
    - Implementa lógica de negócio para gerenciamento de posts
    - Integra validações e tratamento de exceções

---
+++ src/main/java/com/linsvitor/blogging_api/service/PostService.java
@@ -0,0 +1,49 @@
+package com.linsvitor.blogging_api.service;
+
+import com.linsvitor.blogging_api.domain.Post;
+import com.linsvitor.blogging_api.repository.PostRepository;
+import com.linsvitor.blogging_api.service.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public List<Post> search(String term) {
+        IO.println("Buscando por: " + term);
+        return postRepository.search(term);
+    }
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Post insert(Post post) {
+        return postRepository.insert(post);
+    }
+
+    public Post update(String id, Post post) {
+        if (postRepository.existsById(id)) {
+            Post uptadedPost = findById(id);
+            uptadedPost.setTitle(post.getTitle());
+            uptadedPost.setContent(post.getContent());
+            uptadedPost.setCategory(post.getCategory());
+            uptadedPost.setTag(post.getTags());
+            uptadedPost.setUpdatedAt(LocalDateTime.now());
+            postRepository.save(uptadedPost);
+            return uptadedPost;
+        }
+        throw new ResourceNotFoundException(id);
+    }
+}
